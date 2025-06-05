9c350741dfc79631b18fa5595a8f0428871cd8b0
gargi-0912 <gargigupta09122003@gmail.com>
2025-06-05 11:58:15 +0530

    store and carts functionality

--- carts/apps.py
+++ carts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StoreConfig(AppConfig):
+class CartsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'store'
+    name = 'carts'
--- greatkart/views.py
+++ greatkart/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from store.models import Product
 def home(request):
-    products=Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True)
 
-    context={
-        'products':products,
+    context = {
+        'products': products,
     }
     return render(request, 'home.html', context)
