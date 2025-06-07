e4b7e3c496bae6e34ceca94c08a5510742091cb0
gargi-0912 <140802693+gargi-0912@users.noreply.github.com>
2025-06-07 10:28:22 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,6 +143,6 @@ MESSAGE_TAGS = {
 #SMTP configurations
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='ginni921910@gmail.com'
-EMAIL_HOST_PASSWORD="lvqwwewmcxucyaoy"
+EMAIL_HOST_USER='host user email'
+EMAIL_HOST_PASSWORD="host user password"
 EMAIL_USE_TLS=True
